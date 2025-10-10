1bb50566c4cbb168f4c5a33d50d2a00d4b9edfe5
Vaibhav L <loharvb@gmail.com>
2025-10-10 17:39:17 +0530

    Created new component for task-items..

---
+++ src/app/components/task-item/task-item.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'task-item',
+  imports: [],
+  templateUrl: './task-item.html',
+  styleUrl: './task-item.scss'
+})
+export class TaskItem {
+
+}
