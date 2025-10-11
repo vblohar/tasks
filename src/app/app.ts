d199c3d9f4ec4c20afdf99d1d9053f768c5a12ed
Vaibhav L <loharvb@gmail.com>
2025-10-11 19:39:57 +0530

    routes impliment and defined rout for app component..

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskItem } from "./components/task-item/task-item";
 import { TaskForm } from "./components/task-form/task-form";
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskItem, TaskForm],
+  imports: [RouterOutlet, TaskItem, TaskForm, Header],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
