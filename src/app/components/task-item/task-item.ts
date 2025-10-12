a8647cea7bb3ba82293fdce844ba87047ba50704
Vaibhav L <loharvb@gmail.com>
2025-10-12 23:47:17 +0530

    Task items component implemented..

--- src/app/components/task-item/task-item.ts
+++ src/app/components/task-item/task-item.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Task } from '../../models/task';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'task-item',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './task-item.html',
   styleUrl: './task-item.scss'
 })
 export class TaskItem {
+  task = input<Task>();
+  onEdit = output<Task>();
+  onDelete = output<number>();
+  onToggle = output<Task>();
 
+  onToggleComplete() {
+    const updatedTask = { 
+      ...this.task(), 
+      completed: !this.task()?.completed 
+    };
+    // this.onToggle.emit(updatedTask);
+  }
 }
