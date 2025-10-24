2e8e3c96d8ebf5b0e5bc5868896bce9beafb2a0b
Vaibhav L <loharvb@gmail.com>
2025-10-24 23:52:33 +0530

    Declared static tasks and defined get method..

--- src/app/services/task-service.ts
+++ src/app/services/task-service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Task } from '../models/task';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,27 @@ import { inject, Injectable } from '@angular/core';
 export class TaskService {
   private http = inject(HttpClient);
   private readonly API_URL = 'https://jsonplaceholder.typicode.com';
+
+  private MockTasks: Task[] = [
+   { 
+    id: 1,
+    title: 'Learn Angular',
+    description: 'Studt Standalone Angular Component',
+    completed: true,
+    createdAt: new Date('2025-01-01')
+   },
+    { 
+    id: 2,
+    title: 'Build Project',
+    description: 'Create a complete angular project',
+    completed: false,
+    createdAt: new Date('2025-01-02')
+   }
+   
+  ] 
+
+  getTasks(): Observable<Task[]>{
+    return of(this.MockTasks);
+  }
   
 }
